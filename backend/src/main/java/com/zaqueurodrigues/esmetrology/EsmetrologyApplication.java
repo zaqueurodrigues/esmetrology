6a30078c8b699a2f78eb9dde32a6861372b39c00
zaqueurodrigues <zakeurodrigues@outlook.com>
2021-08-19 08:45:38 -0300

    Backend project created

---
+++ backend/src/main/java/com/zaqueurodrigues/esmetrology/EsmetrologyApplication.java
@@ -0,0 +1,13 @@
+package com.zaqueurodrigues.esmetrology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmetrologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsmetrologyApplication.class, args);
+	}
+
+}
