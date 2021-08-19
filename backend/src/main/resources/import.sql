fb063c8e739c0c4ced6734739539e3946f059e99
zaqueurodrigues <zakeurodrigues@outlook.com>
2021-08-19 09:17:29 -0300

    Role, User and Notification entities + data seed

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_user (name, enrollment, email, password) VALUES ('Zaqueu Rodrigues', '588941826', 'zaqueu.pereira@esmaltec.com.br', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, enrollment, email, password) VALUES ('Pedro Gomes', '588949517', 'pedro.junior@esmaltec.com.br', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, enrollment, email, password) VALUES ('Francisco Wedersen', '588919293', 'francisco.wedersen@esmaltec.com.br', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_COMMON');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MAINTAINER');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 3);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
