6d22349e64e495b32216940fdb8ae39e9c3d9b3a
zaqueu.rodrigues <zakeurodrigues@outlook.com>
2022-08-28 18:50:50 -0300

    add postgresql configuration

--- backend/src/main/java/com/zaqueurodrigues/esmetrology/EsmetrologyApplication.java
+++ backend/src/main/java/com/zaqueurodrigues/esmetrology/EsmetrologyApplication.java
@@ -3,8 +3,6 @@ package com.zaqueurodrigues.esmetrology;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.zaqueurodrigues.esmetrology.log.EsmetrologyLogger;
-
 @SpringBootApplication
 public class EsmetrologyApplication {
 	
@@ -13,10 +11,6 @@ public class EsmetrologyApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(EsmetrologyApplication.class, args);
-		
-		EsmetrologyLogger.info(EsmetrologyApplication.class, "teste", null);
-		EsmetrologyLogger.error(EsmetrologyApplication.class, "teste", null);
-		EsmetrologyLogger.debug(EsmetrologyApplication.class, "teste", null);
 	}
 
 }
