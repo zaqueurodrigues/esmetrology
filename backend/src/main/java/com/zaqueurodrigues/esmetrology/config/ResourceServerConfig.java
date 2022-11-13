bf1397c33dd2ac62360350c7f1f0fc6356479235
zaqueurodrigues <zaqueu.rodrigues@vmetrix.com>
2022-11-13 12:24:16 -0300

    Cors configuration and releasing endpoints for tests

--- backend/src/main/java/com/zaqueurodrigues/esmetrology/config/ResourceServerConfig.java
+++ backend/src/main/java/com/zaqueurodrigues/esmetrology/config/ResourceServerConfig.java
@@ -3,14 +3,20 @@ package com.zaqueurodrigues.esmetrology.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -46,8 +52,30 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 			http.headers().frameOptions().disable();
 		}
 
-		http.authorizeRequests().antMatchers(PUBLIC).permitAll().antMatchers(PERMITED_SWAGGER_PATHS).permitAll()
-				.antMatchers(HttpMethod.GET, NORMAL).hasRole("NORMAL").antMatchers(ADMIN).hasRole("ADMIN").anyRequest()
-				.authenticated();
+		http.authorizeRequests().antMatchers("/**").permitAll().antMatchers(PERMITED_SWAGGER_PATHS).permitAll();
+				/*.antMatchers(HttpMethod.GET, NORMAL).hasRole("NORMAL").antMatchers(ADMIN).hasRole("ADMIN").anyRequest()
+				.authenticated();*/
 	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration corsConfig = new CorsConfiguration();
+	    corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+	    corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+	    corsConfig.setAllowCredentials(true);
+	    corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+	 
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", corsConfig);
+	    return source;
+	}
+	 
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+	    FilterRegistrationBean<CorsFilter> bean
+	            = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+	    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	    return bean;
+	}
+
 }
